90f0a0ef336cc27cf79ae4669979736730bb989f
Augustus Murphy <gusbmurphy@gmail.com>
2025-08-28 16:45:26 -0500

    Create `HelloTest`

--- acceptance/build.gradle.kts
+++ acceptance/build.gradle.kts
@@ -10,3 +10,7 @@ dependencies {
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
