054482b1a4c551ec9a61b6907a9d9760c9a52c67
Augustus Murphy <gusbmurphy@gmail.com>
2025-09-14 21:04:06 -0500

    Add test for successful checkout
    This also uses a wildcard import for JUnit `Assertions`

--- spring-boot-adm/src/test/java/fun/gusmurphy/library/springbootadm/service/BookServiceTest.java
+++ spring-boot-adm/src/test/java/fun/gusmurphy/library/springbootadm/service/BookServiceTest.java
@@ -6,7 +6,6 @@ import fun.gusmurphy.library.springbootadm.domain.CheckoutRecord;
 import fun.gusmurphy.library.springbootadm.repository.BookRepository;
 import fun.gusmurphy.library.springbootadm.repository.CheckoutRecordRepository;
 import fun.gusmurphy.library.springbootadm.time.ClockService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -15,10 +14,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.ZonedDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class BookServiceTest {
@@ -28,6 +30,7 @@ class BookServiceTest {
     @Mock BookRepository bookRepository;
 
     @Captor ArgumentCaptor<Book> bookCaptor;
+    @Captor ArgumentCaptor<CheckoutRecord> checkoutRecordCaptor;
 
     @InjectMocks BookService service;
 
@@ -45,8 +48,8 @@ class BookServiceTest {
         verify(bookRepository, times(1)).save(bookCaptor.capture());
 
         var capturedBook = bookCaptor.getValue();
-        Assertions.assertEquals("some-isbn", capturedBook.getIsbn());
-        Assertions.assertEquals(7, capturedBook.getCheckoutTimeInDays());
+        assertEquals("some-isbn", capturedBook.getIsbn());
+        assertEquals(7, capturedBook.getCheckoutTimeInDays());
     }
 
     @Test
@@ -56,7 +59,7 @@ class BookServiceTest {
 
         var result = service.getBookByIsbn("my-isbn");
 
-        Assertions.assertEquals(returnedBook, result);
+        assertEquals(returnedBook, result);
     }
 
     @Test
@@ -65,6 +68,27 @@ class BookServiceTest {
 
         var result = service.checkoutBook("my-isbn", "user-id");
 
-        Assertions.assertFalse(result);
+        assertFalse(result);
+    }
+
+    @Test
+    void checkingOutAnAvailableBookCreatesARecordInTheRepository() {
+        when(checkoutRecordRepository.findByBookIsbn("my-isbn")).thenReturn(Collections.emptyList());
+
+        var bookFromRepo = new Book();
+        when(bookRepository.getReferenceById("my-isbn")).thenReturn(bookFromRepo);
+
+        var testTime = ZonedDateTime.now();
+        when(clockService.currentTime()).thenReturn(testTime);
+
+        var result = service.checkoutBook("my-isbn", "user-id");
+
+        verify(checkoutRecordRepository, times(1)).save(checkoutRecordCaptor.capture());
+        var savedRecord = checkoutRecordCaptor.getValue();
+
+        assertTrue(result);
+        assertEquals(bookFromRepo, savedRecord.getBook());
+        assertEquals("user-id", savedRecord.getUserId());
+        assertEquals(testTime, savedRecord.getCheckoutTime());
     }
 }
