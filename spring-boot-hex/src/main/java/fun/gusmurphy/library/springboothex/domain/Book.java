8da6f6f4f552f5d8e794ab865ae75d758d4c3d1d
Augustus Murphy <gusbmurphy@gmail.com>
2025-10-17 14:49:40 -0500

    Change `Book` to a `class`

--- spring-boot-hex/src/main/java/fun/gusmurphy/library/springboothex/domain/Book.java
+++ spring-boot-hex/src/main/java/fun/gusmurphy/library/springboothex/domain/Book.java
@@ -1,3 +1,20 @@
 package fun.gusmurphy.library.springboothex.domain;
 
-public record Book(Isbn isbn, int checkoutTimeInDays) {}
+public class Book {
+
+    private final Isbn isbn;
+    private final int checkoutTimeInDays;
+
+    public Book(Isbn isbn, int checkoutTimeInDays) {
+        this.isbn = isbn;
+        this.checkoutTimeInDays = checkoutTimeInDays;
+    }
+
+    public Isbn isbn() {
+        return isbn;
+    }
+
+    public int checkoutTimeInDays() {
+        return checkoutTimeInDays;
+    }
+}
