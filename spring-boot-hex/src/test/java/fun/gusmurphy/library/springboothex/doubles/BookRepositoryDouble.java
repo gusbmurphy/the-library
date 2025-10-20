cdb116d9923fceee19806ce9e3416cee4640a43c
Augustus Murphy <gusbmurphy@gmail.com>
2025-10-20 12:08:12 -0500

    fixup! Change in-memory repositories to use `Set`s

--- spring-boot-hex/src/test/java/fun/gusmurphy/library/springboothex/doubles/BookRepositoryDouble.java
+++ spring-boot-hex/src/test/java/fun/gusmurphy/library/springboothex/doubles/BookRepositoryDouble.java
@@ -5,6 +5,6 @@ import fun.gusmurphy.library.springboothex.adapter.persistence.SetBookRepository
 public class BookRepositoryDouble extends SetBookRepository {
 
     public void clear() {
-        bookList.clear();
+        bookSet.clear();
     }
 }
