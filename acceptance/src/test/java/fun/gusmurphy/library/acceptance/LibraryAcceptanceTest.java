cc940a6401da41bd9c010d2718e77fcce6606427
Augustus Murphy <gusbmurphy@gmail.com>
2025-11-11 20:10:25 -0600

    Add `BasicFunctionality` nested tests

--- acceptance/src/test/java/fun/gusmurphy/library/acceptance/LibraryAcceptanceTest.java
+++ acceptance/src/test/java/fun/gusmurphy/library/acceptance/LibraryAcceptanceTest.java
@@ -5,6 +5,7 @@ import fun.gusmurphy.library.acceptance.fixture.OverdueNotificationFixture;
 import fun.gusmurphy.library.acceptance.fixture.UserFixture;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class LibraryAcceptanceTest {
@@ -13,49 +14,52 @@ public class LibraryAcceptanceTest {
     private final OverdueNotificationFixture overdueNotifications =
             new OverdueNotificationFixture();
 
-    @Test
-    void newBookCanBeCheckedOut() throws Exception {
-        var newBook = books.newBookArrives();
-        var user = UserFixture.newUser();
-        var result = user.attemptsToCheckout(newBook);
-        result.checkoutIsSuccessful();
-    }
+    @Nested
+    class BasicFunctionality {
+        @Test
+        void newBookCanBeCheckedOut() throws Exception {
+            var newBook = books.newBookArrives();
+            var user = UserFixture.newUser();
+            var result = user.attemptsToCheckout(newBook);
+            result.checkoutIsSuccessful();
+        }
 
-    @Test
-    void unknownBookCannotBeCheckedOut() throws Exception {
-        var unknownBook = books.unknownBook();
-        var user = UserFixture.newUser();
-        var result = user.attemptsToCheckout(unknownBook);
-        result.bookNotFound();
-    }
+        @Test
+        void unknownBookCannotBeCheckedOut() throws Exception {
+            var unknownBook = books.unknownBook();
+            var user = UserFixture.newUser();
+            var result = user.attemptsToCheckout(unknownBook);
+            result.bookNotFound();
+        }
 
-    @Test
-    void bookCantBeCheckedOutByTwoUsers() throws Exception {
-        var book = books.newBookArrives();
+        @Test
+        void bookCantBeCheckedOutByTwoUsers() throws Exception {
+            var book = books.newBookArrives();
 
-        var firstUser = UserFixture.newUser();
-        var secondUser = UserFixture.newUser();
+            var firstUser = UserFixture.newUser();
+            var secondUser = UserFixture.newUser();
 
-        var firstResult = firstUser.attemptsToCheckout(book);
-        firstResult.checkoutIsSuccessful();
+            var firstResult = firstUser.attemptsToCheckout(book);
+            firstResult.checkoutIsSuccessful();
 
-        var secondResult = secondUser.attemptsToCheckout(book);
-        secondResult.failedBecauseBookIsAlreadyCheckedOut();
-    }
+            var secondResult = secondUser.attemptsToCheckout(book);
+            secondResult.failedBecauseBookIsAlreadyCheckedOut();
+        }
 
-    @Test
-    void aMessageIsSentWhenABookIsOverdue() throws Exception {
-        var checkoutTime = ZonedDateTime.of(1991, 12, 14, 12, 0, 0, 0, ZoneId.systemDefault());
-        TimeTravel.to(checkoutTime);
+        @Test
+        void aMessageIsSentWhenABookIsOverdue() throws Exception {
+            var checkoutTime = ZonedDateTime.of(1991, 12, 14, 12, 0, 0, 0, ZoneId.systemDefault());
+            TimeTravel.to(checkoutTime);
 
-        var book = books.newBookWith().checkoutTimeInDays(90).create();
-        var user = UserFixture.newUser();
-        user.successfullyChecksOut(book);
+            var book = books.newBookWith().checkoutTimeInDays(90).create();
+            var user = UserFixture.newUser();
+            user.successfullyChecksOut(book);
 
-        var lateThreshold = checkoutTime.plusDays(90);
-        overdueNotifications.noneExistFor(book, user, lateThreshold);
+            var lateThreshold = checkoutTime.plusDays(90);
+            overdueNotifications.noneExistFor(book, user, lateThreshold);
 
-        TimeTravel.to(lateThreshold);
-        overdueNotifications.oneExistsFor(book, user, lateThreshold);
+            TimeTravel.to(lateThreshold);
+            overdueNotifications.oneExistsFor(book, user, lateThreshold);
+        }
     }
 }
