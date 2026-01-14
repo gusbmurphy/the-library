1081806782ef43d1b5533c3485cc9125398bbe85
Augustus Murphy <gusbmurphy@gmail.com>
2026-01-13 22:04:35 -0600

    Make request to register user from `UserFixture`

--- acceptance/src/test/java/fun/gusmurphy/library/acceptance/fixture/UserFixture.java
+++ acceptance/src/test/java/fun/gusmurphy/library/acceptance/fixture/UserFixture.java
@@ -10,8 +10,10 @@ import java.util.UUID;
 
 public class UserFixture {
 
-    public static User newUser() {
-        return User.random();
+    public static User newUser() throws Exception {
+        var user = User.random();
+        user.register();
+        return user;
     }
 
     public static User unregisteredUser() {
@@ -31,6 +33,30 @@ public class UserFixture {
 
         public String id() {
             return id;
+        }
+
+        private void register() throws Exception {
+            var requestBodyJson = String.format("{ \"id\": \"%s\" }", id);
+
+            var request =
+                    HttpRequest.newBuilder()
+                            .uri(new URI("http://localhost:8080/users"))
+                            .header("Content-Type", "application/json")
+                            .POST(HttpRequest.BodyPublishers.ofString(requestBodyJson))
+                            .build();
+
+            HttpResponse<String> response =
+                    HttpClient.newBuilder()
+                            .build()
+                            .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() != 201) {
+                throw new RuntimeException(
+                        "Failed to register user. Status: "
+                                + response.statusCode()
+                                + ", Body: "
+                                + response.body());
+            }
         }
 
         public void successfullyChecksOut(Book book) throws Exception {
